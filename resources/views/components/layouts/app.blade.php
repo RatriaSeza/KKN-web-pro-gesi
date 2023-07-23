3f50a0823d0370684da077d8305c27560ce83c3a
RatriaSeza <satriareza2002@gmail.com>
2023-07-23 16:09:42 +0700

    add carousel and card news

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,9 +9,12 @@
     <link rel="icon" href="{{ asset('img/sragen-logo.png') }}">
 
     @vite('resources/css/app.css')
+
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
 </head>
 <body class="font-Primary">
-    <header class="w-full shadow-md py-4 ">
+    <header class="bg-white w-full shadow-md py-4 fixed top-0 z-50">
         <div class="flex justify-between items-center container max-w-5xl mx-auto">
             <a href="#" class="flex items-center">
                 <img class="h-10" src="{{ asset('img/sragen-logo.png') }}" alt="Sragen">
@@ -21,20 +24,27 @@
                 </div>
             </a>
             <nav class="text-primary font-semibold text-sm flex justify-between items-center gap-20">
-                <a class="hover:text-secondary" href="#">Beranda</a>
-                <a class="hover:text-secondary" href="#">Informasi</a>
-                <a class="hover:text-secondary" href="#">Perangkat Desa</a>
-                <a class="text-primary hover:text-white border border-secondary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer">Login</a>
+                <a class="hover:text-secondary ease-out duration-150" href="#">Beranda</a>
+                <a class="hover:text-secondary ease-out duration-150" href="#">Informasi</a>
+                <a class="hover:text-secondary ease-out duration-150" href="#">Perangkat Desa</a>
+                <a class="text-primary hover:text-white border border-secondary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer ease-out duration-150">Login</a>
             </nav>
         </div>
     </header>
 
-    {{ $slot }}
+    <div class="bg-white">
+        {{ $slot }}
+    </div>
 
-    <footer>
+    <footer class="mt-20">
         <div class="container bg-secondary h-96">
 
         </div>
     </footer>
+
+    {{-- JS --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+    {{ $customJS }}
 </body>
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,49 @@
 <x-layouts.app>
     <x-slot:title>
         Desa Gesi | Beranda
-    </x-slot>
+        </x-slot>
+
+        <div>
+            <div class="owl-carousel">
+                <img class="w-full h-screen object-cover" src="{{ asset('img/carousel/kantor-lurah.jpg') }}"
+                    alt="Kantor Lurah">
+            </div>
+        </div>
+
+        <div>
+            <p class="my-10 text-center text-2xl font-semibold">Berita</p>
+
+            <div class="flex justify-center gap-6 max-w-5xl mx-auto">
+                <div class="bg-white max-w-sm rounded overflow-hidden shadow-lg">
+                    <img class="w-full h-40 object-cover" src="{{ asset('img/carousel/kantor-lurah.jpg') }}" alt="Kantor Lurah">
+                    <div class="px-4 py-3">
+                        <div class="font-bold text-lg mb-2">The Coldest Sunset</div>
+                        <p class="text-gray-700 text-sm line-clamp-3">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores
+                            et perferendis eaque, exercitationem praesentium nihil.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <x-slot:customJS>
+            <script>
+                $(document).ready(function() {
+                    $(".owl-carousel").owlCarousel({
+                        slideSpeed: 300,
+                        paginationSpeed: 400,
+
+                        autoplay: true,
+
+                        items: 1,
+                        itemsDesktop: false,
+                        itemsDesktopSmall: false,
+                        itemsTablet: false,
+                        itemsMobile: false,
+                        loop: true,
+                    });
+                });
+            </script>
+            </x-slot>
 </x-layouts.app>
