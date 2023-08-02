e8e121ca3832d25e26b0c23651bbc97b61e57f03
RatriaSeza <satriareza2002@gmail.com>
2023-08-03 01:32:55 +0700

    feat: create database

---
+++ app/Models/Statistic.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Statistic extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'summary',
+        'published',
+        'content',
+    ];
+}
